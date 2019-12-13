419b31a2caef0cfe9d29d82446d19013addf49fb
taha uygun <tahauygun@gmail.com>
2019-12-12 23:15:38 -0800

    some updates

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import Routes from './routes';
-
+import {ApolloClient, createNetworkInterface} from 'react-apollo';
 
 const App = <Routes/>;
 
